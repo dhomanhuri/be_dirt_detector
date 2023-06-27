3c97d141ba6b3cd6052fa455a77333773e22141c
dhomanhurimtm <dhomanhuri@mtm.id>
2023-06-27 21:48:40 +0700

    ..

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker build -t be_dirt_detector-app:latest .
-docker run -d -p 8123:5000  -v /var/www/html/be_dirt_detector:/usr/src/app --name be_dirt_detector be_dirt_detector-app:latest
+docker run -d -p 1895:5000  -v /var/www/html/be_dirt_detector:/usr/src/app --name be_dirt_detector be_dirt_detector-app:latest
